5ddbd52fbe1fe58bfe2e476396690a670f550a23
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2024-04-26 18:21:46 +0300

    modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import { Header } from './modules/header/Header';
+import { Header } from './modules/Header/Header';
+import { Works } from './modules/Works/Works';
+import { Hero } from './modules/Hero/Hero';
+import { Contacts } from './modules/Contacts/Contacts';
+import { Footer } from './modules/Footer/Footer';
 
 function App() {
 	return (
 		<div>
 			<Header />
+			<Hero />
+			<Works />
+			<Contacts />
+			<Footer />
 		</div>
 	);
 }
--- src/modules/header/Header.jsx
+++ src/modules/header/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import style from './Header.module.scss';
-import { NavLink } from 'react-router-dom';
 
 export const Header = () => {
 	return (
