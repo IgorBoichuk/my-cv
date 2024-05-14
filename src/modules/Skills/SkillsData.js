b42f4069eabb0c2e52015a29741c0317c9df885b
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2024-05-14 13:31:02 +0300

    MySkills

---
+++ src/modules/Skills/SkillsData.js
@@ -0,0 +1,23 @@
+export const MySkills = [
+	{
+		title: 'React',
+	},
+	{
+		title: 'NextJS',
+	},
+	{
+		title: 'JAVASCRIPT',
+	},
+	{
+		title: 'TYPESCRIPT',
+	},
+	{
+		title: 'TAILWIND',
+	},
+	{
+		title: 'HTML5',
+	},
+	{
+		title: 'SCSS',
+	},
+];
